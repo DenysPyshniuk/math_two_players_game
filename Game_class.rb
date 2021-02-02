1309a1deca91597e7e93cd851b432d5d84073c42
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-02-01 21:57:41 -0800

    Separate game code

--- Game_class.rb
+++ Game_class.rb
@@ -1,24 +1,47 @@
-require "./Player_class"
-
 class Game
-  puts  "Start Game"
+  puts "Hello Players"
 
-# puts "Let's see how good you are in MATH..."
+  def initialize
+  @player1 = Players.new
+  @player2 = Players.new
+  new_game
+  end
 
-player_1 = Player.new
-puts "What is your name player 1?"
-player_1.name = gets.chomp
-player_2 = Player.new
-puts "And what is your name player 2?"
-player_2.name = gets.chomp
-# puts "#{player_1.name}: #{player_1.lives}/3 lives"
-# puts "#{player_2.name}: #{player_2.lives}/3 lives"
+# GET NAMES OF THE PLAYERS
+  def new_game
+    puts "What is your name Player #1"
+    print "> "
+    @player1.name = gets.chomp
+    puts "What is your name Player #2"
+    @player2.name = gets.chomp
+    puts  "----- Lets Start The Game -----"
 
-game = Game.new
-curent_player = player_1
-
-def change_player
-  curent_player == player_1 ? curent_player = player_2 : player_1
-end
-
+# START OF THE GAME WITH QUESTIONS
+    player_change = Questions.new
+    while @player1.lives > 0 && @player2.lives > 0
+# PLAYER #1 WILL ANSWER FIRST AND SO ON
+      player_change.question(@player1)
+# SCORE
+      puts "#{@player1.name}: #{@player1.lives}/3 vs #{@player2.name}: #{@player2.lives}/3"
+# NEXT PLAYER WILL ANSWER
+      puts "-------- NEXT PLAYER --------"
+      if @player1.lives > 0
+      player_change.question(@player2)
+# SCORE
+      puts "P1: #{@player1.lives}/3 vs P2: #{@player2.lives}/3"
+      puts "-------- NEXT PLAYER --------"
+      end
+    end
+# END OF THE GAME PLAYER 1 LOST
+    if @player1.lives === 0
+      puts "#{@player2.name} wins with a score of #{@player2.lives}/3"
+      puts "-------- GAME OVER --------"
+      puts "Good bye!"
+    else
+# END OF THE GAME PLAYER 2 LOST
+      puts "#{@player1.name} wins with a score of #{@player1.lives}/3"
+      puts "-------- GAME OVER --------"
+      puts "Good bye!"
+    end
+  end
 end
