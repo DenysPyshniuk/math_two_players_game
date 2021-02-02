bcb774d3df97719dd0465f7318c2c3a4e0a9d74f
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-02-01 19:03:11 -0800

    Fix code for one player

--- main.rb
+++ main.rb
@@ -3,32 +3,33 @@ require "./player_class"
 player_1 = Player.new
 player_2 = Player.new
 
-while player_1.lives > 0 do
+while player_1.lives > 0 || player_2.lives >0 do
 
-#Random Number
-num1 = rand(1..20)
-num2 = rand(1..20)
-question = ""
-answer = num1 + num2
+  #Random Number
+  num1 = rand(20)
+  num2 = rand(20)
+  question = ""
+  answer = num1 + num2
 
-# QUESTIONS
-puts "What is the answer for this equation?"
-puts "#{num1} + #{num2} ="
+  # QUESTIONS
+  puts "What is the answer ?"
+  puts "#{num1} + #{num2} ="
 
-# ANSWERS
-player_answer = $stdin.gets.chomp
-# puts "Your answer is: #{player_answer}"
+  # ANSWERS
+  print "> "
+  player_answer = gets.chomp
+  # puts "Your answer is: #{player_answer}"
 
-if answer == player_answer.to_i
-  puts "Yes! You are the best."
-  puts "You kept your #{player_1.lives} lives"
-else
-  puts "Oh no! Not exactly..."
-  puts "The right answer was #{answer}."
-  player_1.lives = player_1.lives - 1
-  puts " You have #{player_1.lives} lives"
+    if answer == player_answer.to_i
+      puts "Yes! You are the best."
+      puts "You kept your #{player_1.lives} lives"
+    else
+      puts "Oh no! Not exactly..."
+      puts "The right answer was #{answer}."
+      player_1.lives -= 1
+      puts " You have #{player_1.lives} lives"
 
-end
+    end
 end
 
 
